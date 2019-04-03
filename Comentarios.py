c790ba8ffac82a6c69d8bdebfd4e5c4d088ad85c
Osvaldo de Oliveira <osvaldo.lvr@gmail.com>
2019-04-03 20:12:23 -0300

    Iniciando nosso projeto

---
+++ Comentarios.py
@@ -0,0 +1,19 @@
+'''
+Script de comentários
+
+Curso de Python
+
+Osvaldo de Oliveira
+'''
+print('Mensagem na tela')
+print("Outra mensagem")
+
+# Exibir um inteiro
+print(3)
+
+#E xibir uma soma
+print(3+5)
+
+print('3+5' + '=8') # Agregando strings
+
+print('9875+4745=', 9875+4745)
